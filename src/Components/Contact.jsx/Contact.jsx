a0d1b06fb909e7255c0b9ee0c4bbef3518213d18
jzf21 <jozefneelamkavil2003@gmail.com>
2024-08-09 10:40:35 +0530

    commit

--- src/Components/Contact.jsx/Contact.jsx
+++ src/Components/Contact.jsx/Contact.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="flex flex-col justify-center items-center p-peach bgblack min-h-[100vh] leading-10 gap-10">
-      <h2 className="text-4xl md:text-7xl p-peach text-center">Want to Connect?<br/>Say Hi!</h2>
+      <h2 className="text-4xl md:text-7xl p-peach text-center">
+        Want to Connect?
+        <br />
+        Say Hi!
+      </h2>
       {/* <h2 className="text-7xl p-peach">Say Hi!</h2> */}
-      <button className=' font-bold text-2xl md:text-3xl rounded-none bg-yellow '>START A CONVERSATION</button>
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jozefantonyneelamkavil.mec@gmail.com&su=Iwouldliketoconnect&body=BODY">
+        <button className=" font-bold text-2xl md:text-3xl rounded-none bg-yellow ">
+          START A CONVERSATION
+        </button>
+      </a>
       <div className="flex flex-row gap-4 "></div>
     </div>
   );
